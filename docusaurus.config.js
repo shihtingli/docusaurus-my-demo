b188189631c3dc7a34ba1f73e0276f16f473ab73
silvia <clarinet221@gmail.com>
2021-08-10 22:21:48 +0800

    add versions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,7 +45,7 @@ module.exports = {
         {
           type: 'docsVersionDropdown',
           position: 'left',
-          dropdownItemsAfter: [{to: '/versions', label: 'All versions'}],
+          //dropdownItemsAfter: [{to: '/versions', label: 'All versions'}],
           dropdownActiveClassDisabled: true,
         },
         {
